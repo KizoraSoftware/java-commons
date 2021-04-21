ab462588c6444d15fbfaea2986746ceddcf75d5a
abhishek-pandey-kizora <47553250+abhishek-pandey-kizora@users.noreply.github.com>
2021-04-21 13:29:17 +0530

    Create DateDifference.java

---
+++ Timelib/src/main/java/com/core/DateDifference.java
@@ -0,0 +1,65 @@
+package com.core;
+
+import java.util.Date;
+
+public class DateDifference {
+
+    public static Date getDifference(String fromDate, String toDate) {
+        Date diffDate = new Date();
+
+        return diffDate;
+    }
+
+    public static Date getDifference(Date fromDate, Date toDate) {
+        Date diffDate = new Date();
+
+        return diffDate;
+    }
+
+    public static Double getDifferenceHours(String fromDate, String toDate) {
+        Double diffDate = 0.0;
+
+        return diffDate;
+    }
+
+    public static Double getDifferenceHours(Date fromDate, Date toDate) {
+        Double diffDate = 0.0;
+
+        return diffDate;
+    }
+
+    public static Double getDifferenceMinutes(String fromDate, String toDate) {
+        Double diffDate = 0.0;
+
+        return diffDate;
+    }
+
+    public static Double getDifferenceMinutes(Date fromDate, Date toDate) {
+        Double diffDate = 0.0;
+
+        return diffDate;
+    }
+
+    public static Long getDifferenceSeconds(String fromDate, String toDate) {
+        Long diffDate = 0L;
+
+        return diffDate;
+    }
+
+    public static Long getDifferenceSeconds(Date fromDate, Date toDate) {
+        Long diffDate = 0L;
+
+        return diffDate;
+    }
+    public static Long getDifferenceMilliSeconds(String fromDate, String toDate) {
+        Long diffDate = 0L;
+
+        return diffDate;
+    }
+
+    public static Long getDifferenceMilliSeconds(Date fromDate, Date toDate) {
+        Long diffDate = 0L;
+
+        return diffDate;
+    }
+}
