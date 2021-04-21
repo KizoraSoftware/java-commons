d6939729a30596f3aa1b607e3c25462dccd71ae7
Jayshree Kshatri <jayshree.kshatri@kizora.com>
2021-04-21 09:27:37 +0100

    Update DateFormat.java

--- Timelib/src/main/java/com/core/DateFormat.java
+++ Timelib/src/main/java/com/core/DateFormat.java
@@ -2,4 +2,8 @@ package com.core;
 
 public class DateFormat {
 
+    public static Boolean isLeapYear(String date){
+        LocalDate localDate = LocalDate.parse(date);
+        return localDate.isLeapYear();
+    }
 }
