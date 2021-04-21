ba3df4e3e09a150b2a4cc685513e707fb8c4c9b7
Radhika kothari <radhika.kothari@kizora.com>
2021-04-21 09:23:11 +0100

    Revert "Update DateFormat.java"
    This reverts commit 7f1b5eb73389bf6067670ce32b618119484b2865.

--- Timelib/src/main/java/com/core/DateFormat.java
+++ Timelib/src/main/java/com/core/DateFormat.java
@@ -1,8 +1,5 @@
 package com.core;
 
 public class DateFormat {
-    public static Boolean isLeapYear(String date){
-        LocalDate dt1 = LocalDate.parse(date);
-        return dt1.isLeapYear();
-    }
+
 }
