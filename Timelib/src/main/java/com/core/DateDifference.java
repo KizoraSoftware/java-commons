bcc3ac18edf1ba0d7d1867e8fc5592aec3233a5d
abhishek-pandey-kizora <47553250+abhishek-pandey-kizora@users.noreply.github.com>
2021-04-22 20:58:46 +0530

    Update DateDifference.java

--- Timelib/src/main/java/com/core/DateDifference.java
+++ Timelib/src/main/java/com/core/DateDifference.java
@@ -3,99 +3,92 @@ package com.core;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
 
 
 public class DateDifference {
 
-    public static LocalDateTime getDifference(String fromDate, String toDate) {
-        LocalDateTime  from = LocalDateTime.parse(fromDate);
-        LocalDateTime  to = LocalDateTime.parse(toDate);
-        return getDifference(from,to);
+    public static Duration getDifference(String fromDate, String toDate) {
+        LocalDateTime from = LocalDateTime.parse(fromDate);
+        LocalDateTime to = LocalDateTime.parse(toDate);
+        return getDifference(from, to);
     }
 
-    public static LocalDateTime getDifference(LocalDateTime fromDate, LocalDateTime toDate) {
-
-        return Duration gap = Duration.between(time2, time1);;
+    public static Duration getDifference(String fromDate, String toDate, ZoneOffset offset) {
+        LocalDateTime from = LocalDateTime.parse(fromDate).atOffset(offset).toLocalDateTime();
+        LocalDateTime to = LocalDateTime.parse(toDate).atOffset(offset).toLocalDateTime();
+        return getDifference(from, to);
     }
 
-    public static LocalDateTime getDifference(Long fromDate, Long toDate) {
-        Date diffDate = new Date();
+    public static Duration getDifference(Long fromDate, Long toDate) {
+        LocalDateTime from = LocalDateTime.ofEpochSecond(fromDate, 0, ZoneOffset.UTC);
+        LocalDateTime to = LocalDateTime.ofEpochSecond(toDate, 0, ZoneOffset.UTC);
+        return getDifference(from, to);
+    }
 
-        return diffDate;
+    public static Duration getDifference(Long fromDate, Long toDate, ZoneOffset offset) {
+        LocalDateTime from = LocalDateTime.ofEpochSecond(fromDate, 0, offset);
+        LocalDateTime to = LocalDateTime.ofEpochSecond(toDate, 0, offset);
+        return getDifference(from, to);
+    }
+
+    public static Duration getDifference(LocalDateTime fromDate, LocalDateTime toDate) {
+        return Duration.between(fromDate, fromDate);
     }
 
     public static Double getDifferenceHours(String fromDate, String toDate) {
-        Double diffDate = 0.0;
-
-        return diffDate;
+        Duration gap = getDifference(fromDate, toDate);
+        return (gap.getSeconds() / 3600.0);
     }
 
-    public static Double getDifferenceHours(Date fromDate, Date toDate) {
-        Double diffDate = 0.0;
-
-        return diffDate;
+    public static Double getDifferenceHours(Long fromDate, Long toDate) {
+        Duration gap = getDifference(fromDate, toDate);
+        return (gap.getSeconds() / 3600.0);
     }
 
-    public static Date getDifferenceHours(Long fromDate, Long toDate) {
-        Date diffDate = new Date();
-
-        return diffDate;
+    public static Double getDifferenceHours(LocalDateTime fromDate, LocalDateTime toDate) {
+        Duration gap = getDifference(fromDate, toDate);
+        return (gap.getSeconds() / 3600.0);
     }
 
     public static Double getDifferenceMinutes(String fromDate, String toDate) {
-        Double diffDate = 0.0;
-
-        return diffDate;
+        Duration gap = getDifference(fromDate, toDate);
+        return (gap.getSeconds() / 60.0);
     }
 
-    public static Double getDifferenceMinutes(Date fromDate, Date toDate) {
-        Double diffDate = 0.0;
-
-        return diffDate;
+    public static Double getDifferenceMinutes(Long fromDate, Long toDate) {
+        Duration gap = getDifference(fromDate, toDate);
+        return (gap.getSeconds() / 60.0);
     }
 
-    public static Date getDifferenceMinutes(Long fromDate, Long toDate) {
-        Date diffDate = new Date();
-
-        return diffDate;
+    public static Double getDifferenceMinutes(LocalDateTime fromDate, LocalDateTime toDate) {
+        Duration gap = getDifference(fromDate, toDate);
+        return (gap.getSeconds() / 60.0);
     }
 
     public static Long getDifferenceSeconds(String fromDate, String toDate) {
-        Long diffDate = 0L;
-
-        return diffDate;
+        return getDifference(fromDate, toDate).getSeconds();
     }
 
-    public static Long getDifferenceSeconds(Date fromDate, Date toDate) {
-        Long diffDate = 0L;
-
-        return diffDate;
+    public static Long getDifferenceSeconds(Long fromDate, Long toDate) {
+        return getDifference(fromDate, toDate).getSeconds();
     }
 
-    public static Date getDifferenceSeconds(Long fromDate, Long toDate) {
-        Date diffDate = new Date();
-
-        return diffDate;
+    public static Long getDifferenceSeconds(LocalDateTime fromDate, LocalDateTime toDate) {
+        return getDifference(fromDate, toDate).getSeconds();
     }
 
     public static Long getDifferenceMilliSeconds(String fromDate, String toDate) {
-        Long diffDate = 0L;
-
-        return diffDate;
+        return getDifference(fromDate, toDate).get(ChronoUnit.MILLIS);
     }
 
-    public static Long getDifferenceMilliSeconds(Date fromDate, Date toDate) {
-        Long diffDate = 0L;
-
-        return diffDate;
+    public static Long getDifferenceMilliSeconds(Long fromDate, Long toDate) {
+        return getDifference(fromDate, toDate).get(ChronoUnit.MILLIS);
     }
 
-    public static Date getDifferenceMilliSeconds(Long fromDate, Long toDate) {
-        Date diffDate = new Date();
-
-        return diffDate;
+    public static Long getDifferenceMilliSeconds(LocalDateTime fromDate, LocalDateTime toDate) {
+        return getDifference(fromDate, toDate).get(ChronoUnit.MILLIS);
     }
+
 }
