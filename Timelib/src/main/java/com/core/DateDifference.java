b38e65723e703ed6c606230c436cb293b586bb9e
abhishek-pandey-kizora <47553250+abhishek-pandey-kizora@users.noreply.github.com>
2021-04-22 22:04:39 +0530

    Update DateDifference.java

--- Timelib/src/main/java/com/core/DateDifference.java
+++ Timelib/src/main/java/com/core/DateDifference.java
@@ -1,9 +1,7 @@
 package com.core;
 
 
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
+import java.time.*;
 import java.time.temporal.ChronoUnit;
 
 
@@ -21,6 +19,12 @@ public class DateDifference {
         return getDifference(from, to);
     }
 
+    public static Duration getDifference(String fromDate, String toDate, String zoneName) {
+        LocalDateTime from = LocalDateTime.parse(fromDate);
+        LocalDateTime to = LocalDateTime.parse(toDate);
+        return getDifference(from, to, zoneName);
+    }
+
     public static Duration getDifference(Long fromDate, Long toDate) {
         LocalDateTime from = LocalDateTime.ofEpochSecond(fromDate, 0, ZoneOffset.UTC);
         LocalDateTime to = LocalDateTime.ofEpochSecond(toDate, 0, ZoneOffset.UTC);
@@ -30,7 +34,29 @@ public class DateDifference {
     public static Duration getDifference(Long fromDate, Long toDate, ZoneOffset offset) {
         LocalDateTime from = LocalDateTime.ofEpochSecond(fromDate, 0, offset);
         LocalDateTime to = LocalDateTime.ofEpochSecond(toDate, 0, offset);
-        return getDifference(from, to);
+        return getDifference(from, to, offset);
+    }
+
+    public static Duration getDifference(Long fromDate, Long toDate, String zoneName) {
+        LocalDateTime now = LocalDateTime.now();
+        ZoneId zone = ZoneId.of(zoneName);
+        ZoneOffset offset = zone.getRules().getOffset(now);
+        LocalDateTime from = LocalDateTime.ofEpochSecond(fromDate, 0, offset);
+        LocalDateTime to = LocalDateTime.ofEpochSecond(toDate, 0, offset);
+        return getDifference(from, to, zoneName);
+    }
+
+    public static Duration getDifference(LocalDateTime fromDate, LocalDateTime toDate, ZoneOffset offset) {
+        OffsetDateTime from = fromDate.atOffset(offset);
+        OffsetDateTime to = toDate.atOffset(offset);
+        return Duration.between(from, to);
+    }
+
+    public static Duration getDifference(LocalDateTime fromDate, LocalDateTime toDate, String zoneName) {
+        ZoneId zoneId = ZoneId.of(zoneName);
+        ZonedDateTime from = fromDate.atZone(zoneId);
+        ZonedDateTime to = toDate.atZone(zoneId);
+        return Duration.between(from, to);
     }
 
     public static Duration getDifference(LocalDateTime fromDate, LocalDateTime toDate) {
@@ -38,33 +64,75 @@ public class DateDifference {
     }
 
     public static Double getDifferenceHours(String fromDate, String toDate) {
-        Duration gap = getDifference(fromDate, toDate);
-        return (gap.getSeconds() / 3600.0);
+        return (getDifferenceSeconds(fromDate, toDate) / 3600.0);
     }
 
     public static Double getDifferenceHours(Long fromDate, Long toDate) {
-        Duration gap = getDifference(fromDate, toDate);
-        return (gap.getSeconds() / 3600.0);
+        return (getDifferenceSeconds(fromDate, toDate) / 3600.0);
     }
 
     public static Double getDifferenceHours(LocalDateTime fromDate, LocalDateTime toDate) {
-        Duration gap = getDifference(fromDate, toDate);
-        return (gap.getSeconds() / 3600.0);
+        return (getDifferenceSeconds(fromDate, toDate) / 3600.0);
+    }
+
+    public static Double getDifferenceHours(String fromDate, String toDate, ZoneOffset offset) {
+        return (getDifferenceSeconds(fromDate, toDate, offset) / 3600.0);
+    }
+
+    public static Double getDifferenceHours(Long fromDate, Long toDate, ZoneOffset offset) {
+        return (getDifferenceSeconds(fromDate, toDate, offset) / 3600.0);
+    }
+
+    public static Double getDifferenceHours(LocalDateTime fromDate, LocalDateTime toDate, ZoneOffset offset) {
+        return (getDifferenceSeconds(fromDate, toDate, offset) / 3600.0);
+    }
+
+    public static Double getDifferenceHours(String fromDate, String toDate, String zoneName) {
+        return (getDifferenceSeconds(fromDate, toDate, zoneName) / 3600.0);
+    }
+
+    public static Double getDifferenceHours(Long fromDate, Long toDate, String zoneName) {
+        return (getDifferenceSeconds(fromDate, toDate, zoneName) / 3600.0);
+    }
+
+    public static Double getDifferenceHours(LocalDateTime fromDate, LocalDateTime toDate, String zoneName) {
+        return (getDifferenceSeconds(fromDate, toDate, zoneName) / 3600.0);
     }
 
     public static Double getDifferenceMinutes(String fromDate, String toDate) {
-        Duration gap = getDifference(fromDate, toDate);
-        return (gap.getSeconds() / 60.0);
+        return (getDifferenceSeconds(fromDate, toDate) / 60.0);
     }
 
     public static Double getDifferenceMinutes(Long fromDate, Long toDate) {
-        Duration gap = getDifference(fromDate, toDate);
-        return (gap.getSeconds() / 60.0);
+        return (getDifferenceSeconds(fromDate, toDate) / 60.0);
     }
 
     public static Double getDifferenceMinutes(LocalDateTime fromDate, LocalDateTime toDate) {
-        Duration gap = getDifference(fromDate, toDate);
-        return (gap.getSeconds() / 60.0);
+        return (getDifferenceSeconds(fromDate, toDate) / 60.0);
+    }
+
+    public static Double getDifferenceMinutes(String fromDate, String toDate, String zoneName) {
+        return (getDifferenceSeconds(fromDate, toDate, zoneName) / 60.0);
+    }
+
+    public static Double getDifferenceMinutes(Long fromDate, Long toDate, String zoneName) {
+        return (getDifferenceSeconds(fromDate, toDate, zoneName) / 60.0);
+    }
+
+    public static Double getDifferenceMinutes(LocalDateTime fromDate, LocalDateTime toDate, String zoneName) {
+        return (getDifferenceSeconds(fromDate, toDate, zoneName) / 60.0);
+    }
+
+    public static Double getDifferenceMinutes(String fromDate, String toDate, ZoneOffset offset) {
+        return (getDifferenceSeconds(fromDate, toDate, offset) / 60.0);
+    }
+
+    public static Double getDifferenceMinutes(Long fromDate, Long toDate, ZoneOffset offset) {
+        return (getDifferenceSeconds(fromDate, toDate, offset) / 60.0);
+    }
+
+    public static Double getDifferenceMinutes(LocalDateTime fromDate, LocalDateTime toDate, ZoneOffset offset) {
+        return (getDifferenceSeconds(fromDate, toDate, offset) / 60.0);
     }
 
     public static Long getDifferenceSeconds(String fromDate, String toDate) {
@@ -79,6 +147,30 @@ public class DateDifference {
         return getDifference(fromDate, toDate).getSeconds();
     }
 
+    public static Long getDifferenceSeconds(String fromDate, String toDate, String zoneName) {
+        return getDifference(fromDate, toDate, zoneName).getSeconds();
+    }
+
+    public static Long getDifferenceSeconds(Long fromDate, Long toDate, String zoneName) {
+        return getDifference(fromDate, toDate, zoneName).getSeconds();
+    }
+
+    public static Long getDifferenceSeconds(LocalDateTime fromDate, LocalDateTime toDate, String zoneName) {
+        return getDifference(fromDate, toDate, zoneName).getSeconds();
+    }
+
+    public static Long getDifferenceSeconds(String fromDate, String toDate, ZoneOffset offset) {
+        return getDifference(fromDate, toDate, offset).getSeconds();
+    }
+
+    public static Long getDifferenceSeconds(Long fromDate, Long toDate, ZoneOffset offset) {
+        return getDifference(fromDate, toDate, offset).getSeconds();
+    }
+
+    public static Long getDifferenceSeconds(LocalDateTime fromDate, LocalDateTime toDate, ZoneOffset offset) {
+        return getDifference(fromDate, toDate, offset).getSeconds();
+    }
+
     public static Long getDifferenceMilliSeconds(String fromDate, String toDate) {
         return getDifference(fromDate, toDate).get(ChronoUnit.MILLIS);
     }
@@ -91,4 +183,27 @@ public class DateDifference {
         return getDifference(fromDate, toDate).get(ChronoUnit.MILLIS);
     }
 
+    public static Long getDifferenceMilliSeconds(String fromDate, String toDate, String zoneName) {
+        return getDifference(fromDate, toDate, zoneName).get(ChronoUnit.MILLIS);
+    }
+
+    public static Long getDifferenceMilliSeconds(Long fromDate, Long toDate, String zoneName) {
+        return getDifference(fromDate, toDate, zoneName).get(ChronoUnit.MILLIS);
+    }
+
+    public static Long getDifferenceMilliSeconds(LocalDateTime fromDate, LocalDateTime toDate, String zoneName) {
+        return getDifference(fromDate, toDate, zoneName).get(ChronoUnit.MILLIS);
+    }
+
+    public static Long getDifferenceMilliSeconds(String fromDate, String toDate, ZoneOffset offset) {
+        return getDifference(fromDate, toDate, offset).get(ChronoUnit.MILLIS);
+    }
+
+    public static Long getDifferenceMilliSeconds(Long fromDate, Long toDate, ZoneOffset offset) {
+        return getDifference(fromDate, toDate, offset).get(ChronoUnit.MILLIS);
+    }
+
+    public static Long getDifferenceMilliSeconds(LocalDateTime fromDate, LocalDateTime toDate, ZoneOffset offset) {
+        return getDifference(fromDate, toDate, offset).get(ChronoUnit.MILLIS);
+    }
 }
