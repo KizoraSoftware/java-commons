0f2ea64d7ef8572c89e0921d710c000840bb7967
Jayshree Kshatri <jayshree.kshatri@kizora.com>
2021-04-23 08:19:56 +0100

    Update DateFormat.java

--- Timelib/src/main/java/com/core/DateFormat.java
+++ Timelib/src/main/java/com/core/DateFormat.java
@@ -1,5 +1,15 @@
 package com.core;
 
 public class DateFormat {
+    public static int weekofTheYear(String date){
+        String format = "yyyy-MM-dd";
 
+        SimpleDateFormat df = new SimpleDateFormat(format);
+        Date pdate = df.parse(date);
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(pdate);
+        int week = cal.get(Calendar.WEEK_OF_YEAR);
+        return week;
+    }
 }
