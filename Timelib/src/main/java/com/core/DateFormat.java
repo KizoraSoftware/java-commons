e2353ccaa9669806ddcd040d3b9f2ca0025b7bd1
Jayshree Kshatri <jayshree.kshatri@kizora.com>
2021-04-23 08:15:49 +0100

    Update DateFormat.java

--- Timelib/src/main/java/com/core/DateFormat.java
+++ Timelib/src/main/java/com/core/DateFormat.java
@@ -1,5 +1,9 @@
 package com.core;
 
 public class DateFormat {
-
+    public int dayofTheYear(String date){
+        LocalDate localDate = LocalDate.parse(date);
+        int day = localDate.getDayOfYear();
+        return day;
+    }
 }
