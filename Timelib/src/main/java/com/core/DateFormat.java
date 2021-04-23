3f0f796345c69ec800a44bf46fa5e758ce1c56a2
Jayshree Kshatri <jayshree.kshatri@kizora.com>
2021-04-23 08:19:15 +0100

    Update DateFormat.java

--- Timelib/src/main/java/com/core/DateFormat.java
+++ Timelib/src/main/java/com/core/DateFormat.java
@@ -1,5 +1,10 @@
 package com.core;
 
 public class DateFormat {
-
+   public static String lastDateofTheMonth(int year, int month) {
+       YearMonth yearMonth = YearMonth.of(year, month);
+       LocalDate last = yearMonth.atEndOfMonth();
+       return last.toString;
+   }
+   
 }
